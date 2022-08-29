d6edf85d0ffbedcaac8e0ecae2cf64930df7b7eb
Kate <knaychuk@gmail.com>
2022-08-29 11:08:39 -0700

    initial cleanup & pages setup

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBdVeGPQBoPvb0_DBnGg4knwuZbtqALXQs",
+  authDomain: "expire-172fd.firebaseapp.com",
+  projectId: "expire-172fd",
+  storageBucket: "expire-172fd.appspot.com",
+  messagingSenderId: "294018044881",
+  appId: "1:294018044881:web:eb1589e980d748afa286f0"
+};
+
+initializeApp(firebaseConfig)
+
