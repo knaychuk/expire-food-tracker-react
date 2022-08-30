677af32813e3bd0145d9072511bbaa981d6f10aa
Kate <knaychuk@gmail.com>
2022-08-30 04:06:40 -0700

    cleanup modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,12 +1,22 @@
 
+import { useState } from 'react'
 import styles from './Modal.module.css'
 
 
 export default function Modal({ setShowModal }) {
+	const [name, setName] = useState('')
+	const [category, setCategory] = useState('')
+	const [quantity, setQuantity] = useState(0)
+	const [expiryDate, setExpiryDate] = useState('')
+
 
 	const handleSubmit = () => {
 		setShowModal(false)
-		
+		addDocument({
+			name,
+			category, 
+			quantity, 
+			expiryDate})
 	}
 
 	return (
@@ -15,16 +25,44 @@ export default function Modal({ setShowModal }) {
 				<h2>Add Item</h2>
 				<form onSubmit={handleSubmit}>
 					<label>
-						<span>Name</span>
-						<input type="text" />
+						<span>Item Name:</span>
+						<input 
+							type="text" 
+							onChange={(e) => setName(e.target.value)}
+							value={name}
+							/>
 					</label>
 					<label>
-						<span>Name</span>
-						<input type="text" />
+						<span>Category:</span>
+						<select 
+							onChange={(e) => setCategory(e.target.value)}>
+							<option value="fruits">Fruits</option>
+							<option value="vegetables">Vegetables</option>
+							<option value="dairy">Dairy</option>
+							<option value="meat">Meat</option>
+							<option value="grains">Grains</option>
+							<option value="canned goods">Canned Goods</option>
+							<option value="dry foods">Dry Foods</option>
+							<option value="condiments/seasonings">Condiments/Seasonings</option>
+							<option value="snacks">Snacks</option>
+							<option value="other">Other</option>
+						</select>
 					</label>
 					<label>
-						<span>Name</span>
-						<input type="text" />
+						<span>Quantity:</span>
+						<input 
+							type="number" 
+							onChange={(e) => setQuantity(e.target.value)}
+							value={quantity}	
+						/>
+					</label>
+					<label>
+						<span>Expiry Date:</span>
+						<input 
+							type="date" 
+							onChange={(e) => setExpiryDate(e.target.value)}
+							value={expiryDate}
+						/>
 					</label>
 					<div className={styles.button}>
 						<button>Add</button>
